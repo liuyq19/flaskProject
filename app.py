85f22206c60b1832b039bc7e8fff2f81ab8898ce
jarvisyqliu <jarvisyqliu@tencent.com>
2024-01-29 17:57:32 +0800

    202401291757

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
+class User:
+    def __init__(self,username,email):
+        self.username = username
+        self.email = email
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!111222'
+def index():
+    hobby = "游戏"
+    person = {
+        "name": "张三",
+        "age": 181
+    }
+    user = User("李四", "xx@qq.com")
+    return render_template("index.html", hobby=hobby, person=person, user=user)
 
 
 if __name__ == '__main__':
